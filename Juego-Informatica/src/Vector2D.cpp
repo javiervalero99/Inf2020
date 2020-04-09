55d8e64608fae8a053642508fb35b545c078ff05
orgynmobs <62205153+orgynmobs@users.noreply.github.com>
2020-04-09 18:37:50 +0200

    Recuperacion Completa del proyecto inicial

---
+++ Juego-Informatica/src/Vector2D.cpp
@@ -0,0 +1,44 @@
+#include "Vector2D.h"
+
+Vector2D::Vector2D(float xv, float yv)
+{
+}
+
+Vector2D::~Vector2D()
+{
+}
+
+float Vector2D::modulo()
+{
+	return 0.0f;
+}
+
+float Vector2D::argumento()
+{
+	return 0.0f;
+}
+
+Vector2D Vector2D::Unitario()
+{
+	return Vector2D();
+}
+
+Vector2D Vector2D::operator-(Vector2D&)
+{
+	return Vector2D();
+}
+
+Vector2D Vector2D::operator+(Vector2D&)
+{
+	return Vector2D();
+}
+
+float Vector2D::operator*(Vector2D&)
+{
+	return 0.0f;
+}
+
+Vector2D Vector2D::operator*(float)
+{
+	return Vector2D();
+}
