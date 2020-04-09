47b5e39ae34ee8e2b0b3d17c02d40515eb20c1f7
orgynmobs <62205153+orgynmobs@users.noreply.github.com>
2020-04-09 16:47:38 +0200

    actualizacion de clases

---
+++ Juego-Informatica/src/Mundo.h
@@ -0,0 +1,18 @@
+#include "Personaje.h"
+class Mundo
+{
+public: 
+	void Tecla(unsigned char key);
+	void Inicializa();
+	void RotarOjo();
+	void Mueve(float t);
+	void Dibuja();
+	void TeclaUp(unsigned char key);
+	void TeclaDown(unsigned char key);
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+	Personaje hombre;
+
+};
