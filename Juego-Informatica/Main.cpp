5a92e8f3be8b438852c2d33d4757510b7ea7f339
orgynmobs <62205153+orgynmobs@users.noreply.github.com>
2020-04-09 12:58:09 +0200

    actualizacion restart

---
+++ Juego-Informatica/Main.cpp
@@ -0,0 +1,20 @@
+#include "glut.h"
+
+void Dibujar() {
+
+	glFlush();
+}
+
+int main(int argc, char* args[]) {
+	glutInit(&argc, args);
+
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowSize(800, 60);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow("Trabajo de informatica.");	
+
+	glutDisplayFunc(Dibujar);
+	glutMainLoop();
+
+	return 0;
+}
--- src/Plataformas.cpp
+++
@@ -1,2 +0,0 @@
-#include "Plataformas.h"
-
