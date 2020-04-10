cef73d6f614576dc4253717be8ea0dd31380095f
javiervalero99 <javier.valero.cuadrado@alumnos.upm.es>
2020-04-10 19:31:50 +0200

    Camara y salto

--- Juego-Informatica/src/Mundo.cpp
+++ Juego-Informatica/src/Mundo.cpp
@@ -12,7 +12,7 @@ void Mundo::RotarOjo()
 }
 void Mundo::Dibuja()
 {
-	gluLookAt(x_ojo, y_ojo, z_ojo,  // posicion del ojo
+	gluLookAt(hombre.GetPosicion().x, hombre.GetPosicion().y, z_ojo,  // posicion del ojo
 			hombre.GetPosicion().x, hombre.GetPosicion().y, 0.0, // hacia que punto mira  (donde este el personaje, quizas crear una clase camara) 
 			0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
 
