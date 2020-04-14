13ae6037fce46a70bd0a1ac8f0e54315b864ef85
orgynmobs <62205153+orgynmobs@users.noreply.github.com>
2020-04-14 12:33:33 +0200

    sistema de mov: maquina de estados

--- Juego-Informatica/src/Personaje.h
+++ Juego-Informatica/src/Personaje.h
@@ -9,6 +9,7 @@ public:
 	int r, q; //r otorga la validación para cada operacion de direccion
 	//q cambia la sensibilidad de desplazamiento cuando saltas (permite movimientos más rapidos)
 	float s, m; //s es el incremento cuando TeclaDown y m es el incremento cuando TeclaUp (contario a TeclaDown)
+	int estados[2] = { 0 ,0 }; //maquina de estados para a y d
 	bool valid_salto;
 	int i = 0;
 	Personaje();
